b718f0b9728bf1434752fcfa76847dbb31bb87f1
MINSOO KANG <stengine2@gmail.com>
2020-07-15 17:25:24 +0900

    testing I2C data exchange

--- I2Ctest/test.py
+++ I2Ctest/test.py
@@ -14,12 +14,12 @@ numb = 1
 
 print ("Enter 1 for ON or 0 for OFF")
 while numb == 1:
-
-    ledstate = input(">>>>   ")
+    """ledstate = input(">>>>   ")
 
     if ledstate == "1":
         bus.write_byte(addr, 0x1) # switch it on
     elif ledstate == "0":
         bus.write_byte(addr, 0x0) # switch it on
     else:
-        numb = 0
+        numb = 0"""
+    print(bus.read_byte(0x8),end="hey\n\n")
