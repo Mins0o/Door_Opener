709e2b21928e1036e3500b0e7c476fa44804eeed
MINSOO KANG <stengine2@gmail.com>
2020-07-17 18:37:52 +0900

    Serial input to async plot

--- SerialComm/SerialComm.ino
+++ SerialComm/SerialComm.ino
@@ -1,18 +1,32 @@
 #include <Wire.h>
-int prev = 500;
-int pres;
-byte oneByte;
+int prev3=5;
+int prev2 =507;
+int prev = 507;
+float pres =507;
 int soundRead;
-
+int count=0;
 void setup(){
   Serial.begin(9600);
 }
 
 void loop(){
   soundRead=analogRead(A3);
-  pres=prev*0.6+soundRead*0.4;
-  oneByte=(byte)(pres/4);
-  Serial.write(oneByte);
-  delay(9);
-  prev=pres;
+  //pres=soundRead*0.93+prev*0.07;
+  if(abs(soundRead-(prev+prev2+prev3+2)/3)>4){//threshold
+    count=400;
+  }
+  if(count>0){
+    /*
+    Serial.write(highByte(prev3));
+    Serial.write(lowByte(prev3));
+    Serial.write('\r');
+    Serial.write('\n');*/
+    byte sendMsg[3] = {highByte(prev3),lowByte(prev3),'\n'};
+    Serial.write(sendMsg,3);
+    count--;
+  }
+  prev3=prev2;
+  prev2=prev;
+  //prev=pres;
+  prev=soundRead;
 }
--- SerialComm/SerialRecieve.py
+++ SerialComm/SerialRecieve.py
@@ -1,19 +1,104 @@
 import serial
 from os import listdir
 from os.path import isfile, join
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import asyncio
+import time
 
-devices = [f for f in listdir("/dev/") if ((f[:4]=="ttyA" or f[:4] == "ttyU"))]
+"""async def plot(data):
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(data)
+    plt.show()"""
 
-for i in range(len(devices)):
-    print(i,end="\t")
-    print(devices[i])
+fig=plt.figure()
+ax = fig.add_subplot(1,1,1)
+buffer=[]
 
-print("Which device are we using?")
+def chooseDevice():
+    devices = [f for f in listdir("/dev/") if ((f[:4]=="ttyA" or f[:4] == "ttyU"))]
+    for i in range(len(devices)):
+        print(i,end="\t")
+        print(devices[i])
+    print("Which device are we using?")
+    selection=int(input())
+    arduino = serial.Serial("/dev/"+devices[selection],9600)
+    return arduino
 
-selection=int(input())
+def addData(i):
+    while arduino.in_waiting>0:
+        rawread=arduino.readline()
+        while(len(rawread)<3):
+            rawread+=arduino.read()
+        reading=int.from_bytes(rawread[:2],"big")
+        buffer.append(reading)
+    ax.clear()
+    ax.plot(buffer[-1000:])
 
-arduino = serial.Serial("/dev/"+devices[selection],9600)
 
-while True:
-    soundIn=ord(arduino.read())
-    print(soundIn)
+    
+if __name__=="__main__":
+    arduino=chooseDevice()
+    arduino.readline()
+    arduino.readline()
+    arduino.readline()
+    ani=animation.FuncAnimation(fig,addData,fargs=(),interval=1)
+    plt.show()
+    """
+    while True:
+        buffer=buffer[-20:]
+        while arduino.in_waiting>0:
+            rawread=arduino.readline()
+            while(len(rawread)<3):
+                rawread+=arduino.read()
+            reading=int.from_bytes(rawread[:2],"big")
+            print(reading)
+            buffer.append(reading)
+        if(len(buffer)>0):
+            print(buffer)"""
+        
+        
+"""
+            buffer+=arduino.readline()
+            #plot(buffer)
+        buffer=[]
+        time.sleep(1)
+"""
+"""
+# Create figure for plotting
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+xs = []
+ys = []
+
+# Initialize communication with TMP102
+tmp102.init()
+
+# This function is called periodically from FuncAnimation
+def animate(i, xs, ys):
+
+    # Read temperature (Celsius) from TMP102
+    temp_c = round(tmp102.read_temp(), 2)
+
+    # Add x and y to lists
+    xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+    ys.append(temp_c)
+
+    # Limit x and y lists to 20 items
+    xs = xs[-20:]
+    ys = ys[-20:]
+
+    # Draw x and y lists
+    ax.clear()
+    ax.plot(xs, ys)
+
+    # Format plot
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.30)
+    plt.title('TMP102 Temperature over Time')
+    plt.ylabel('Temperature (deg C)')
+
+# Set up plot to call animate() function periodically
+ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=1000)
+plt.show()"""
